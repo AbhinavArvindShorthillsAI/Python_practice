7e23e07af43ec7a0501405ec12b9c8b36fab680b
AbhinavArvindShorthillsAI <abhinav.arvind@shorthills.ai>
2025-02-24 18:54:41 +0530

    Update 01_variables_datatypes.py

--- 01_variables_datatypes.py
+++ 01_variables_datatypes.py
@@ -1,58 +1,77 @@
-# Variables in Python
-x = 10         # Integer
-y = "Hello"    # String
-z = 3.14       # Float
-a = True       # Boolean
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def arithmetic_operations(self):
+        print("Addition:", self.a + self.b)
+        print("Subtraction:", self.a - self.b)
+        print("Multiplication:", self.a * self.b)
+        print("Division:", self.a / self.b)
+        print("Modulus:", self.a % self.b)
+        print("Exponentiation:", self.a ** self.b)
+        print("Floor Division:", self.a // self.b)
+    
+    def comparison_operations(self):
+        print("Equal:", self.a == self.b)
+        print("Not Equal:", self.a != self.b)
+        print("Greater Than:", self.a > self.b)
+        print("Less Than:", self.a < self.b)
+        print("Greater or Equal:", self.a >= self.b)
+        print("Less or Equal:", self.a <= self.b)
+    
+    def bitwise_operations(self):
+        print("Bitwise AND:", self.a & self.b)
+        print("Bitwise OR:", self.a | self.b)
+        print("Bitwise XOR:", self.a ^ self.b)
+        print("Bitwise NOT (a):", ~self.a)
+        print("Left Shift:", self.a << 2)
+        print("Right Shift:", self.a >> 2)
+    
+    def logical_operations(self, x, y):
+        print("Logical AND:", x and y)
+        print("Logical OR:", x or y)
+        print("Logical NOT:", not x)
+    
+    def assignment_operations(self):
+        self.a += 5
+        print("Updated a (after += 5):", self.a)
 
-# Printing variables
-print(x, y, z, a)
 
-# Checking data types
-print(type(x))  # int
-print(type(y))  # str
-print(type(z))  # float
-print(type(a))  # bool
+# Demonstrating Variable Types
+class VariableDemo:
+    def __init__(self):
+        self.x = 10         # Integer
+        self.y = "Hello"    # String
+        self.z = 3.14       # Float
+        self.a = True       # Boolean
+    
+    def display_variables(self):
+        print(self.x, self.y, self.z, self.a)
+        print("Type of x:", type(self.x))
+        print("Type of y:", type(self.y))
+        print("Type of z:", type(self.z))
+        print("Type of a:", type(self.a))
+
+
+# Execution
+if __name__ == "__main__":
+    print("Variable Demonstration:")
+    var_demo = VariableDemo()
+    var_demo.display_variables()
+    
+    print("\nCalculator Operations:")
+    calc = Calculator(10, 5)
+    calc.arithmetic_operations()
+    calc.comparison_operations()
+    calc.bitwise_operations()
+    calc.logical_operations(True, False)
+    calc.assignment_operations()
 
 
 
 
-# Arithmetic Operators
-a = 10
-b = 5
-print(a + b)   # Addition
-print(a - b)   # Subtraction
-print(a * b)   # Multiplication
-print(a / b)   # Division
-print(a % b)   # Modulus
-print(a ** b)  # Exponentiation
-print(a // b)  # Floor Division
 
-# Comparison Operators
-print(a == b)  # Equal
-print(a != b)  # Not Equal
-print(a > b)   # Greater Than
-print(a < b)   # Less Than
-print(a >= b)  # Greater or Equal
-print(a <= b)  # Less or Equal
-
-# Logical Operators
-x = True
-y = False
-print(x and y)  # Logical AND
-print(x or y)   # Logical OR
-print(not x)    # Logical NOT
-
-# Bitwise Operators
-print(a & b)  # Bitwise AND
-print(a | b)  # Bitwise OR
-print(a ^ b)  # Bitwise XOR
-print(~a)     # Bitwise NOT
-print(a << 2) # Left Shift
-print(a >> 2) # Right Shift
-
-# Assignment Operators
-a += 5  # Equivalent to a = a + 5
-print(a)
 
 
 
